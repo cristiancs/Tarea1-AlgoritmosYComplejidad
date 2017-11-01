6b863225faa35ac0cafe954a3bc07a428fe47cca
Cristian Navarrete <cristiannavarrete@outlook.com>
2017-11-01 17:13:29 -0300

    removida funciona no utilizada, comentar los pasos.

--- main.cpp
+++ main.cpp
@@ -351,26 +351,13 @@ void BFS(Grafo *G,int nodo,int *cvert, int pintura, int* fobias_comunidad, int* 
 
     
 }
-void DFS(Grafo *G,int aux1,int *w,int *cvert, int pintura, int* fobias_comunidad, int* auxiliar_nodos[]){
-    if(G->getMark(aux1,auxiliar_nodos) == -1){
-        fobias_comunidad[G->getFobia(aux1,auxiliar_nodos)]+=1; // Aumentar al contador de fobias
-        G->setMark(aux1, pintura, auxiliar_nodos);
-    }
-    for(*w= G->first(aux1,auxiliar_nodos); *w< G->nVertex()+1; *w = G->next(aux1,auxiliar_nodos)){
-        if(G->getMark(*w,auxiliar_nodos) == -1){
-            (*cvert)++;
-            DFS(G,*w, w, cvert, pintura, fobias_comunidad, auxiliar_nodos);
-        }
-
-    }
-}
 int main(){
     Grafo G;
     int cantidad_datos, aux1,aux2, i, cantidad_vertices;
     cin >> cantidad_datos;
 
     // Agregar Nodos
-    cout << "Agregando nodos"<<"\n";
+    //cout << "Agregando nodos"<<"\n";
     i = 1;
     int **auxiliar_nodos = new int*[cantidad_datos+1]; // Lista auxiliar para agilizar setEdge
     while(i <= cantidad_datos){
@@ -379,7 +366,7 @@ int main(){
         i++;
     }
 
-    cout << "Agregando vertices"<<"\n";
+    //cout << "Agregando vertices"<<"\n";
     // Agregar Vertices
     cin >> cantidad_vertices;
     i = 0;
@@ -400,15 +387,15 @@ int main(){
 
     //int w = 0; // Auxiliar para DFS
     int i2 = 1;
-    cout << "Contabilizando Fobias"<<"\n";
+    //cout << "Contabilizando Fobias"<<"\n";
     while(nodos_visitados <cantidad_datos){
-        cout << "Buscando nodos no visitados"<<"\n";
-        cout << "Visitados: " << nodos_visitados << "de " << cantidad_datos<<endl;
+        //cout << "Buscando nodos no visitados"<<"\n";
+        //cout << "Visitados: " << nodos_visitados << "de " << cantidad_datos<<endl;
         // Buscar Nodo no visitado
         while(G.getMark(i2,auxiliar_nodos) != -1){
             i2++;
         }
-        cout << "Encontrado nodo no visitado " << i2<<"\n";
+        //cout << "Encontrado nodo no visitado " << i2<<"\n";
 
         int* fobias_comunidad = (int*) calloc(10000, sizeof(int)); // Vamos guardando las veces que se ve una fobia
 
@@ -444,7 +431,7 @@ int main(){
 
     }
 
-    cout << "Mostrando salida"<<"\n";
+    //cout << "Mostrando salida"<<"\n";
     cout << cantidad_comunidades << '\n';
     // Liberamos el espacio que quedo en 0 antes de 
     comunidades = (int*) realloc(comunidades, cantidad_comunidades*sizeof(int));
@@ -455,7 +442,7 @@ int main(){
     delete(auxiliar_nodos);
     free(comunidades);
 
-    G.mostrar_grafo();
+    //G.mostrar_grafo();
 
     G.~Grafo();
 
